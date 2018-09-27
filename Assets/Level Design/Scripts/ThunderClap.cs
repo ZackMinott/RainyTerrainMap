fa74e8de6cd20828199f2c822a37061a23a1b042
Zack Minott <zacharyminott1997@gmail.com>
2018-09-27 16:02:00 -0700

    Rainy Terrain

---
+++ Assets/Level Design/Scripts/ThunderClap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderClap : MonoBehaviour {
+	public AudioClip clip;
+	bool canFlicker = true;
+
+	void Update(){
+		StartCoroutine (Flicker ()); //allows the IEnumerator flicker to work
+	}
+
+	IEnumerator Flicker (){
+		if (canFlicker) {
+			canFlicker = false;
+			GetComponent<AudioSource> ().PlayOneShot (clip);
+
+			GetComponent<Light>().enabled = true;
+			yield return new WaitForSeconds (Random.Range (0.1f, 0.4f)); //sets the lenght of the flicker/lighting strike
+			GetComponent<Light>().enabled = false;
+			yield return new WaitForSeconds (Random.Range (0.1f, 5f)); //staggers time between strikes 
+			canFlicker = true;	
+		}
+	}
+}
